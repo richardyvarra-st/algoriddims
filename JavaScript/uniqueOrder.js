92fe15b4643d022ae956d196e9afbd9d1659239e
Richard Yvarra <richard.yvarra@servicetrade.com>
2024-07-22 10:56:25 -0400

    added the comments

--- JavaScript/uniqueOrder.js
+++ JavaScript/uniqueOrder.js
@@ -1,9 +1,14 @@
 var uniqueInOrder=function(iterable){
     //your code here - remember iterable can be a string or an array
     var arr = [];
+    
     for (var i = 0; i < iterable.length; i++) {
+      // loop through the iterable
       if (iterable[i] !== iterable[i + 1]) {
+        // check if there is a repeating letter
+
         arr.push(iterable[i]);
+        // adding the iterable to the array (push)
       }
     }   
     return arr;
